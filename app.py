c94762e2015c82beb2ed4b27d5d89b30938a00df
Hursh Desai <hursh.desai@stern.nyu.edu>
2020-11-21 21:47:43 -0800

    heroku updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from commands import create_tables
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -9,6 +10,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from views import *
+
+app.cli.add_command(create_tables)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-conn_string = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}'.format(
-user = 'postgres', 
-password = 'l,kmazsx1',
-host = 'localhost',
-port = '5432',
-dbname = 'first_database')
+import os
 
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = conn_string
-SECRET_KEY = 'GF9zHFS-cSXQyxN31zCmjg'
-SQLALCHEMY_TRACK_MODIFICATIONS =  False # silence the deprecation warning
+FLASK_ENV = os.getenv('FLASK_ENV')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SQLALCHEMY_TRACK_MODIFICATIONS = False # silence the deprecation warning
+
+
+
