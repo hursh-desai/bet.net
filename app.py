87c4b53110866f626f238eaad49c7cbddba22f98
Hursh Desai <hursh.desai@stern.nyu.edu>
2020-11-21 21:55:11 -0800

    SERVER_NAME update

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ from views import *
 app.cli.add_command(create_tables)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+SERVER_NAME = os.getenv('SERVER_NAME')
 FLASK_ENV = os.getenv('FLASK_ENV')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False # silence the deprecation warning
